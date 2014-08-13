1c5ef99113a40084133b7a90c673312af0d06e37
Carl Markham <doddsey65@hotmail.com>
2014-08-13 18:18:43 +0100

    updated person tests

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -4,7 +4,12 @@ RSpec.describe Person do
 
     it "should throw the name required error" do
         person = Person.create
-        expect(person.errors[:name]).to eq(["Name can't be blank"])
+        expect(person.errors[:name][0]).to eq("can't be blank")
+    end
+
+    it "should throw the name is too short error" do
+        person = Person.create(name: 'C')
+        expect(person.errors[:name][0]).to eq("is too short (minimum is 2 characters)")
     end
 
 end
