2301fb7457fc6301753c546aa7dd0c168e9580ba
Carl Markham <doddsey65@hotmail.com>
2014-08-13 16:06:44 +0100

    added person save test

--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -4,4 +4,10 @@ class PersonTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  # 
+  
+  test "should not save without valid params" do
+    person = Person.create()
+    assert_not person.save
+  end
 end
