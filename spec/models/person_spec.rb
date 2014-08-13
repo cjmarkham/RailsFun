4be63dea27217bbe3b0d19ee660ba907df749f84
Marc Qualie <marc@marcqualie.com>
2014-08-13 18:00:47 +0100

    Update person_spec.rb
    You can simplify this test a lot. Here is my thought process.
    
    1. No need to manually raise an error, it's extra overhead
    2. Asserting that the full array is checked means it will catch any extra validations other people add so they know to test them
    3. Person.save being false is tested at the framework level. If you other (more granular) tests pass, this will pass by design

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -4,15 +4,7 @@ RSpec.describe Person do
 
     it "should throw the name required error" do
         person = Person.create
-
-        expect { raise StandardError, person.errors.full_messages[0]}
-            .to raise_error(StandardError, "Name can't be blank")
-    end
-
-    it "should not create person" do
-        person = Person.create
-
-        expect(person.save).to be false
+        expect(person.errors[:name]).to eq(["Name can't be blank"])
     end
 
 end
